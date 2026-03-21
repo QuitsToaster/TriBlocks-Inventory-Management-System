3187540e3e136c30ba8332c7b5d1565f64d01c69
QuitsToaster <162087509+QuitsToaster@users.noreply.github.com>
2026-03-21 09:06:26 +0800

    fix stocks

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'type',
+        'quantity',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
